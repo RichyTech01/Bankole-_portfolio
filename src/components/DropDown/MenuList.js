9f569a2cb4fa37730272a5404877f993d1d3048b
Bankole Ona ara <richwaydev01@gmail.com>
2023-06-04 16:31:57 -0700

    message

--- src/components/DropDown/MenuList.js
+++ src/components/DropDown/MenuList.js
@@ -13,7 +13,7 @@ const MenuList = ({ closeMenu }) => {
              <motion.ul  initial={{opacity:0, y:-100}}
                  animate={{opacity:1, y:0}}
                  transition={{duration:0.6, ease:'easeInOut'}}
-                 className=" md:hidden absolute right-0 top-0  w-screen h-screen bg-white z-50 flex justify-center">
+                 className=" md:hidden absolute right-0   w-screen h-screen bg-white z-50 flex justify-center">
 
                 <li className="flex flex-col items-center justify-center my-auto space-y-16 text-xl">
                     <Link className="text-black hover:text-gray-500" onClick={handleLinkClick} href={'/'}>
